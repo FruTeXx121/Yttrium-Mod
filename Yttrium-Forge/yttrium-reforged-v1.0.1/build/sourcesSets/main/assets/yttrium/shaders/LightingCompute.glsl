4367720b26e9d2b71d507ad15a6563b588605f5b
FruTeXx <owsihdsnjs@outlook.de>
2025-11-14 10:48:39 +0100

    Delete Yttrium-Forge/yttrium-reforged-v1.0.1 directory

--- Yttrium-Forge/yttrium-reforged-v1.0.1/build/sourcesSets/main/assets/yttrium/shaders/LightingCompute.glsl
+++
@@ -1,8 +0,0 @@
-#version 430
-layout(local_size_x = 16, local_size_y = 16) in;
-layout(std430, binding = 0) buffer LightLevels { float light[]; };
-layout(std430, binding = 1) buffer BlockData { int blocks[]; };
-void main() {
-    uint i = gl_GlobalInvocationID.y * 16 + gl_GlobalInvocationID.x;
-    light[i] *= (blocks[i] == 0 ? 1.0 : 0.5);
-}
--- Yttrium-Forge/yttrium-reforged-v1.0.1/src/main/resources/assets/yttrium/shaders/matrix_multiply.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-
-uniform mat4 matrixA;
-uniform mat4 matrixB;
-
-void main() {
-    mat4 result = matrixA * matrixB;
-    // Use result in rendering or write to buffer
-}
